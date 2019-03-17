879f88884a48725bae482dfd4dfd675594e65684
C.pons <charleypons@live.fr>
2019-03-17 03:18:11 +0100

    comments added

--- menu.js
+++ menu.js
@@ -10,7 +10,8 @@ class Menu //Change the style of the active tab of the menu
 
   }
 
-  activeFirstMenu(){
+  activeFirstMenu()//Mangage the style of the header menu
+  {
     this.container.on("click", function(){
      $(".activemenu li a").removeClass("activeFirstMenu");
      $(this).addClass("activeFirstMenu");
@@ -18,7 +19,8 @@ class Menu //Change the style of the active tab of the menu
    })
   }
 
-  activeSecondMenu(){
+  activeSecondMenu()//Manage the style of the portfolio menu
+  {
     this.secondContainer.on("click", function(){
       $("#navigation2 li a").removeClass("active2menu");
       $(this).addClass("active2menu");
@@ -26,10 +28,11 @@ class Menu //Change the style of the active tab of the menu
     })
 
   }
-  activeSecondMenuArrow(){
+  activeSecondMenuArrow()//Manage the adding of the arrow behind the menu
+  {
     this.thirdContainer.on("click", function(){
       $("#navigation2 li div").removeClass("arrowdown");
-      $(this).next().addClass("arrowdown");
+      $(this).next().addClass("arrowdown");//We use next() for cibling the next element of the current object
     })
   }
 
