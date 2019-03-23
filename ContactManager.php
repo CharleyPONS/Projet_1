64aa3c11998536a68e9360eec7ce366ea762258d
C.pons <charleypons@live.fr>
2019-03-23 18:13:06 +0100

    error corrected

--- ContactManager.php
+++ ContactManager.php
@@ -1,34 +1,35 @@
 <?php
 
 /**
- * Class who manage 
+ * 
  */
 class ContactManager
 {
-	
+	private $_db;
+  
 	public function __construct($db)
 	{
-		$this->setDb($db)
+		$this->setDb($db);
 	}
-}
 
-public function add(Personnage $perso)
+
+public function add(Contact $customer)
   {
-    $req = $this->_db->prepare('INSERT INTO webagency(Name, email, Subject, Message) VALUES(:Name, :email, :Subject, :Message)');
-    $req->bindValue(':nom', $perso->nom());
-    $req->bindValue(':email', $perso->email());
-    $req->bindValue(':Subject', $perso->Subject());
-    $req->bindValue(':Message', $perso->Message());
+    $req = $this->_db->prepare('INSERT INTO contact(Name, email, Subject, Message) VALUES(:Name, :email, :Subject, :Message)');
+    $req->bindValue(':Name', $customer->Name());
+    $req->bindValue(':email', $customer->email());
+    $req->bindValue(':Subject', $customer->Subject());
+    $req->bindValue(':Message', $customer->Message());
 
     $req->execute();
     
     
-    ]);
   }
 
   public function setDb(PDO $db)
   {
     $this->_db = $db;
   }
+}
 
 ?>
